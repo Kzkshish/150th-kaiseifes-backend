114a32682119f85e77853dbe532ad25c7cbf149b
candidate2609 <keigo0827511@gmail.com>
2021-06-21 08:14:24 +0900

    [swap]db splite

--- Sandans/models.py
+++ Sandans/models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import AbstractUser
-from django.contrib.postgres.fields import ArrayField
 from django.db import models
 import uuid
 # Create your models here.
@@ -7,7 +6,7 @@ import uuid
 
 class Sandan(AbstractUser):
     icon = models.ImageField(null=True, upload_to="icons/")
-    links = ArrayField(base_field=models.URLField(), blank=True, null=True)
+    links = models.URLField(blank=True, null=True)
     books = models.FileField(
         upload_to="books/", blank=True, null=True)
     waittime = models.IntegerField(default=0)
--- kaiseifes_150th_backend/settings.py
+++ kaiseifes_150th_backend/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'kaiseifes_150th_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "kaiseifes_150th_backend",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
