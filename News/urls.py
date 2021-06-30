855c2a225ad0682b3694ae0d7c36d8f2304a74a7
candidate2609 <keigo0827511@gmail.com>
2021-06-30 21:27:16 +0900

    [add]config push heroku

--- News/urls.py
+++ News/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 app_name = "News"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('tag/<str:tag>/', views.tag, name='tag'),
-    path('detail/<int:news_id/', views.detail, name='detail'),
     path("api/news/", views.NewsListAPIView.as_view(), name="news_list"),
     path("api/news/<int:pk>",
          views.NewsDetailAPIView.as_view(), name="news_detail"),
--- News/views.py
+++ News/views.py
@@ -11,22 +11,6 @@ from News.serializers import NewsSerializer, TagSerializer
 # Create your views here.
 
 
-def index(request):
-    news = News.objects.order_by('created_datetime')
-    return render(request, 'News/index.html', {'news': news})
-
-
-def tag(request, tag):
-    tags = Tag.objects.get(name=tag)
-    news = News.objects.filter(tag=tag)
-    return render(request, 'News/index.html', {'tag': tags, 'news': news})
-
-
-def detail(request, news_id):
-    news = News.objects.order_by('created_datetime')
-    return render(request, 'News/index.html', {'news': news})
-
-
 class NewsListAPIView(ListAPIView):
     permission_classes = (AllowAny,)
     queryset = News.objects.all()
