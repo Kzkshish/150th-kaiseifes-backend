e793c32d9ee290e78bf68e178a9cc3749f6aa9e7
candidate2609 <keigo0827511@gmail.com>
2021-08-03 11:58:03 +0900

    [change]mail system

--- AdminControl/views.py
+++ AdminControl/views.py
@@ -3,7 +3,7 @@ from django.views.generic import CreateView, View, FormView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.core.mail import send_mass_mail
+from django.core.mail import send_mass_mail, send_mail
 from django.template.loader import render_to_string
 
 from News.models import News
@@ -23,8 +23,9 @@ class NewsCreateFormView(CreateView):
         news = form.save()
         messages.success(self.request, "お知らせを投稿しました。")
 
-        send_mass_mail([(news.title, news.text, "150th KaiseiFes HP", (subscriber.email,))
-                       for subscriber in Subscriber.objects.all()])
+        for subscriber in Subscriber.objects.all():
+            send_mail(news.title, news.text,
+                      "150th KaiseiFes HP", (subscriber.email,), html_message=news.text)
         messages.success(self.request, "メールの送信が完了しました。")
 
         return HttpResponseRedirect(reverse('AdminControl:post_news'))
